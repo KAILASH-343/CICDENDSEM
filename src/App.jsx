f29e5d08f478e0c7ad698a9370ed7ca6915f98ac
Kailash_343 <kailashkodli335@gmail.com>
2025-11-17 17:18:27 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/travelfrontend">
       <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
